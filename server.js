c311181930bf6fd8311d561ffa3e456ccc9602e0
Sheldon Perkins <csperkins18@gmail.com>
2020-07-30 01:22:56 -0400

    display static files

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ var express = require("express");
 var app = express();
 var port = 3030;
 
+// Configure server
+app.configure( function() {
+    //Where to serve static content
+    app.use( express.static( application_root ) );
+});
+
 app.listen(port, function () {
     console.log("Server is running on "+ port +" port");
   });
